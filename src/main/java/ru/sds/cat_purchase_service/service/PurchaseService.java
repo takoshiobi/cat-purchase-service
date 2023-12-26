607b863f3351793c89938ad48344695c85a5dc75
Yulia Scherbakova <yscherbakova@dpd.ru>
2023-12-27 01:19:31 +0300

    error handling

--- src/main/java/ru/sds/cat_purchase_service/service/PurchaseService.java
+++ src/main/java/ru/sds/cat_purchase_service/service/PurchaseService.java
@@ -1,6 +1,7 @@
 package ru.sds.cat_purchase_service.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,7 @@ import java.sql.Timestamp;
 import java.util.List;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class PurchaseService {
 
@@ -54,6 +56,12 @@ public class PurchaseService {
     @RabbitListener(queues = "purchase-cat-response-mq")
     @Transactional
     public void updatePurchase(@Payload PurchaseResponseDto purchaseResponseDto) {
+        List<PurchaseDBParamsDto> purchase = purchaseRepository.findById(purchaseResponseDto.getPurchaseId());
+
+        if (purchase.isEmpty()) {
+            log.warn(String.format("Update failed. Purchase with id %s doesn't exist", purchaseResponseDto.getPurchaseId()));
+        }
+
         purchaseRepository.update(
                 purchaseResponseDto.getPurchaseId(),
                 new Timestamp(System.currentTimeMillis()),
