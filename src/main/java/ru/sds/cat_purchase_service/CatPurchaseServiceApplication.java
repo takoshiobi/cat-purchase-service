b6f9869f8a783372cbfd59706ef8348fcdbf2fd7
Yulia Scherbakova <yscherbakova@dpd.ru>
2023-12-26 21:08:14 +0300

    init

---
+++ src/main/java/ru/sds/cat_purchase_service/CatPurchaseServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.sds.cat_purchase_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatPurchaseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatPurchaseServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sds/cat_purchase_service/CatPurchaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sds.cat_purchase_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatPurchaseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
